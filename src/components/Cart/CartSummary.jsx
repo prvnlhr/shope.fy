818932f548aa723475230bc4cec8a316a7888aa7
Praveen Lohar <praveenlohar.in@gmail.com>
2024-08-16 10:25:12 +0530

    fix: summary card ui sizing

--- src/components/Cart/CartSummary.jsx
+++ src/components/Cart/CartSummary.jsx
@@ -12,7 +12,7 @@ const PriceBadge = ({ price }) => {
       <div className={styles.currencyIconDiv}>
         <CurrencyIcon />
       </div>
-      <div className={styles.priceValueDiv}>
+      <div className={styles.textDiv}>
         <p>{price}</p>
       </div>
     </div>
@@ -28,11 +28,47 @@ const CartSummary = () => {
   };
   return (
     <div className={styles.summaryWrapper}>
-      <div className={styles.summaryHeaderWrapper}>
+      <div className={styles.headerWrapper}>
         <CommonHeading text={"SUMMARY"} />
       </div>
-      <div className={styles.summaryDetailsWrapper}>
+      <div className={styles.mainWrapper}>
         <div className={styles.summaryCard}>
+          <div className={styles.cardRow}>
+            <div className={styles.labelCell}>
+              <p>Subtotal</p>
+            </div>
+            <div className={styles.valueCell}>
+              <PriceBadge price={summaryData.subTotal} />
+            </div>
+          </div>
+          <div className={styles.cardRow}>
+            <div className={styles.labelCell}>
+              <p>Delivery & Handling charges</p>
+            </div>
+            <div className={styles.valueCell}>
+              <PriceBadge price={deliveryHandlingCharges} />
+            </div>
+          </div>
+          <div className={styles.cardRow}>
+            <div className={styles.labelCell}>
+              <p>Total Price</p>
+            </div>
+            <div className={styles.valueCell}>
+              <PriceBadge price={getTotalPrice()} />
+            </div>
+          </div>
+          <div className={styles.checkoutBtnWrapper}>
+            <button type="button" className={styles.checkoutBtn}>
+              <div className={styles.buttonTextDiv}>
+                <p>Checkout</p>
+              </div>
+              <div className={styles.btnIconDiv}>
+                <ArrowIcon color={"white"} />
+              </div>
+            </button>
+          </div>
+        </div>
+        {/* <div className={styles.summaryCard}>
           <div className={styles.rowGroupDiv}>
             <div className={styles.rowLabelDiv}>
               <p>Subtotal</p>
@@ -70,7 +106,7 @@ const CartSummary = () => {
               </div>
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
