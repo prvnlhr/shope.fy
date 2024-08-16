a7643875ae46058f474c7e0f9b1fc4e83bded21f
Praveen Lohar <praveenlohar.in@gmail.com>
2024-08-16 16:01:54 +0530

    chore : some ui changes in loading.js

--- src/app/(public)/cart/loading.js
+++ src/app/(public)/cart/loading.js
@@ -1,7 +1,18 @@
 import React from "react";
-
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p style={{ fontWeight: "500" }}>loading...</p>
+    </div>
+  );
 };
 
 export default loading;
